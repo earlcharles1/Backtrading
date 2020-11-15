97d8f4a7a2145fad4578e9c0dc9ba1789c719740
Earl Charles <cearl424@gmail.com>
2020-11-14 20:15:44 -0600

    commit again

--- bollingerbands.py
+++ bollingerbands.py
@@ -1,44 +1,59 @@
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
-
-import datetime  # For datetime objects
-import os.path  # To manage paths
-import sys  # To find out the script name (in argv[0])
-
-# Import the backtrader platform
+import datetime
+import os.path
+import sys
 import backtrader as bt
-import backtrader.indicators
-import backtrader.feeds as btfeeds
+from backtrader import Indicator
+from backtrader.indicators.mabase import MovAv
+from backtrader.indicators.deviation import StandardDeviation as StdDev
 
-class BollingerBand(bt.Strategy):
 
-    params = (('period', 5),
-        ('devfactor', 3.0),)
+class BollingerBands(bt.Strategy):
+    '''
+    Defined by John Bollinger in the 80s. It measures volatility by defining
+    upper and lower bands at distance x standard deviations
+    Formula:
+      - midband = SimpleMovingAverage(close, period)
+      - topband = midband + devfactor * StandardDeviation(data, period)
+      - botband = midband - devfactor * StandardDeviation(data, period)
+    See:
+      - http://en.wikipedia.org/wiki/Bollinger_Bands
+    '''
+    alias = ('BBands',)
+
+    lines = ('mid', 'top', 'bot',)
+    params = (('period', 5), ('devfactor', 3.0), ('movav', MovAv.Simple),)
+
+    plotinfo = dict(subplot=False)
+    plotlines = dict(
+        mid=dict(ls='--'),
+        top=dict(_samecolor=True),
+        bot=dict(_samecolor=True),
+    )
 
 
     def __init__(self):
-        self.period = self.params.period
-        self.devfactor = self.params.devfactor
-        self.close = self.datas[0].close
-        self.midband = backtrader.indicators.SimpleMovingAverage(self.period)
-        self.topband = self.midband + self.devfactor*StandardDeviation(data,period)
-        self.botband = self.midband - self.devfactor*StandardDeviation(data,period)
+        self.boll=bt.indicators.BollingerBands(period=self.params.period, devfactor=self.params.devfactor)
+        ma = self.params.movav(self.data, period=self.params.period)
+        self.lines.mid = ma
+        stddev = self.p.devfactor * StdDev(self.data, ma, period=self.params.period,
+                                           movav=self.params.movav)
+        self.lines.top = ma + stddev
+        self.lines.bot = ma - stddev
 
     def next(self):
-        self.log('Close, %.2f' % self.close[0])
-        if self.order:
+        if self.position:
             return
         if not self.position:
-            if self.close[0] > self.topband:
-                self.log('Short/Sale, %.2f' % self.close)
+            if self.data.close > self.boll.lines.top:
                 self.order = self.sell()
         else:
-            if self.close[0] < self.botband:
-                self.log('Buy, %.2f' % self.close)
+            if self.data.close < self.boll.lines.bot:
                 self.order = self.buy()
 if __name__ == '__main__':
     cerebro = bt.Cerebro()
-    cerebro.addstrategy(BollingerBand)
+    cerebro.addstrategy(BollingerBands)
     modpath = os.path.dirname(os.path.abspath(sys.argv[0]))
     datapath = os.path.join(modpath, 'RICK.csv')
     data = bt.feeds.YahooFinanceCSVData(
@@ -56,3 +71,4 @@ if __name__ == '__main__':
     print('Starting Portfolio Value: %.2f' % cerebro.broker.getvalue())
     cerebro.run()
     print('Final Portfolio Value: %.2f' % cerebro.broker.getvalue())
+    cerebro.plot()
